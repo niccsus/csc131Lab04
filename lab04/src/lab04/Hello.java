89a3fd87f864d34c1caecee63d9f500b7cc2f3dd
dannyboy <dannyboy@localhost>
2020-11-08 14:30:46 -0800

    Danny - Edited Line 18 with name

--- lab04/src/lab04/Hello.java
+++ lab04/src/lab04/Hello.java
@@ -15,7 +15,7 @@ public class Hello {
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Daniel Perez.");
 		System.out.println("SacStateID - This line was added by Foad Olfat");
 
 }
