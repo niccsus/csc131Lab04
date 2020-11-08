d473ea00e46debd391c1b7e30d176123bb79f085
Foad Olfat <olfatfoad@gmail.com>
2020-11-08 14:39:21 -0800

    Foad committing conflict

--- lab04/src/lab04/Hello.java
+++ lab04/src/lab04/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
-		//Changing line 8
+		//Foad Olfat
 		//Nic Burt
 		//Danny Perez
 		System.out.println("SacStateID - This line was added by First name Last name.");
