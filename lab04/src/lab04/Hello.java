b49716e0263c0f45b0912ee324c76668afef8166
Foad Olfat <olfatfoad@gmail.com>
2020-11-08 14:24:06 -0800

    Foad Second commit

---
+++ lab04/src/lab04/Hello.java
@@ -0,0 +1,22 @@
+package lab04;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		//Changing line 8
+		//Nic Burt
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by Foad Olfat");
+
+}
+
+}
